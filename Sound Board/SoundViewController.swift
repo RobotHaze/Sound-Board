a710c9d03c479de8f9be53623750fa5cec75a8e3
Jordan Matthews <hayleywoodard@Hayleys-MacBook-Pro.local>
2017-04-27 15:54:55 -0400

    First Commit

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Jordan Matthews on 4/27/17.
+//  Copyright © 2017 Jordan Matthews. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+ 
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+}
